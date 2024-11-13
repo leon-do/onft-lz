cd7d8db65899c4e7e05b127493efddc6670af74d
leon-do <leondo123@gmail.com>
2024-11-13 14:54:47 -0500

    tokenURI

--- contracts/ONFT.sol
+++ contracts/ONFT.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.22;
 
 import { ONFT721 } from "@layerzerolabs/onft-evm/contracts/onft721/ONFT721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract ONFT is ONFT721 {
     constructor(
@@ -10,4 +11,9 @@ contract ONFT is ONFT721 {
         address _lzEndpoint,
         address _delegate
     ) ONFT721(_name, _symbol, _lzEndpoint, _delegate) {}
+
+    function tokenURI(uint256 _tokenId) public pure override returns (string memory) {
+        string memory baseURI = "https://hello.com/";
+        return string(abi.encodePacked(baseURI, Strings.toString(_tokenId)));
+    }
 }
